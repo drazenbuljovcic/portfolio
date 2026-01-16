f44cad33460439a1c38a1830415ac362507b4bef
Dražen Buljovčić <drazen.buljovcic@gmail.com>
2026-01-17 00:03:59 +0100

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,18 @@
+export const SITE = {
+  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  author: "Dražen Buljovčić",
+  profile: "https://satnaing.dev/",
+  desc: "Dražen's portfolio",
+  title: "Portfolio - Dražen Buljovčić",
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: true,
+  postPerIndex: 4,
+  postPerPage: 4,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  showArchives: true,
+  showBackButton: true, // show back button in post detail
+  dynamicOgImage: true,
+  dir: "ltr", // "rtl" | "auto"
+  lang: "en", // html lang code. Set this empty and default will be "en"
+  timezone: "Europe/Belgrade", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+} as const;
